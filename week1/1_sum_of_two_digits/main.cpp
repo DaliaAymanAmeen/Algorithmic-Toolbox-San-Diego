7a530999005b67e10e56ba07121459edb42cd8d4
DaliaAymanAmeen <dalia.ayman.ameen@gmail.com>
2020-05-02 06:34:50 +0200

    Create sum_of_two_digits

---
+++ week1/1_sum_of_two_digits/main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin >>x>>y;
+    cout<<x+y;
+
+    return 0;
+}
